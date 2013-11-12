b82e9b88640dd93eee407c99cd7374f6d06b2671
byrnefio <byrnefio@grinnell.edu>
2013-11-12 17:47:48 -0600

    RPN seems to be done.

---
+++ src/taojava/util/RPN.java
@@ -0,0 +1,94 @@
+package taojava.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.String;
+
+public class RPN {
+
+    static LinkedStack<Double> vals;
+    static PrintWriter pen = new PrintWriter(System.out, true);
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+	boolean terminate = false;
+	vals = new LinkedStack<Double>();
+
+	while (!terminate) {
+	    // Take input
+	    try {
+		BufferedReader reader = new BufferedReader(
+			new InputStreamReader(System.in));
+
+		String[] in = reader.readLine().split("\\s+");
+		for (int i = 0; i < in.length; i++) {
+		    terminate = eval(in[i]);
+
+		}
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	}
+    }
+
+    private static boolean eval(String input) throws Exception {
+	double first = 0;
+	double second = 0;
+	LinkedStackIterator<Double> it = new LinkedStackIterator<Double>(vals);
+	if (input.equals("q")) {
+	    return true;
+	} else if (input.equals("p")) {
+	    if (vals.isEmpty()) {
+		pen.println("Stack empty");
+	    } else {
+		pen.println(vals.peek());
+	    }
+	} else if (input.equals("s")) {
+	    while (it.hasNext()) {
+		pen.println(it.next());
+	    }
+	} else if (input.equals("c")) {
+	    vals = new LinkedStack<Double>();	    
+	} else if (input.equals("+") || input.equals("/") || input.equals("*")
+		|| input.equals("-") || input.equals("m")) {
+	    try {
+		first = vals.pop();
+	    } catch (Exception e) {
+		pen.println("Not enough items on stack");
+		return false;
+	    }
+	    try {
+		second = vals.pop();
+	    } catch (Exception e) {
+		vals.push(first);
+		pen.println("Not enough items on stack");
+		return false;
+	    }
+	    if (input.equals("+")) {
+		vals.push(first + second);
+	    } else if (input.equals("-")) {
+		vals.push(first - second);
+	    } else if (input.equals("/")) {
+		vals.push(first / second);
+	    } else if (input.equals("m")){
+		vals.push(Math.max(first, second));
+	    }else{
+		vals.push(first * second);
+	    }
+
+	} else {
+	    try {
+		Double d = Double.parseDouble(input);
+		vals.push(d);
+	    } catch (Exception e) {
+		pen.println("Unsupported operation: " + input);
+	    }
+	}
+
+	return false;
+    }
+}
---
+++ src/taojava/util/RPNexpt.java
@@ -0,0 +1,13 @@
+package taojava.util;
+
+public class RPNexpt {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
