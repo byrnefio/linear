17f3bd28da2558520f960e8aca6cf2102b2780b0
byrnefio <byrnefio@grinnell.edu>
2013-11-12 18:36:27 -0600

    RPN is now functional and waiting to be fully commented. More methods!

--- src/taojava/util/RPN.java
+++ src/taojava/util/RPN.java
@@ -7,47 +7,62 @@ import java.io.PrintWriter;
 import java.lang.String;
 
 public class RPN {
-
+    /**
+     * A flag indicating the user has attempted to terminate the REPL
+     */
+    static boolean terminate = false;
+    
+    /**
+     * A stack of operands
+     */
     static LinkedStack<Double> vals;
-    static PrintWriter pen = new PrintWriter(System.out, true);
 
     /**
      * @param args
      */
     public static void main(String[] args) throws Exception {
-	boolean terminate = false;
 	vals = new LinkedStack<Double>();
+	PrintWriter pen = new PrintWriter(System.out, true);
 
 	while (!terminate) {
-	    // Take input
 	    try {
+		// Take input
 		BufferedReader reader = new BufferedReader(
 			new InputStreamReader(System.in));
-		// Split by whitespace
-		String[] in = reader.readLine().split("\\s+");
-		// Evaluate all the characters on the line 
-		for (int i = 0; i < in.length; i++) {
-		    terminate = eval(in[i]);
-
-		}
+		String input = reader.readLine();
+		// Parse and evaluate input
+		parse(input, pen);
 	    } catch (IOException e) {
 		e.printStackTrace();
-	    }
-	}
-    }
+	    }// try/catch
+	}// while
+    }// main
+
     /**
-     * 
+     * Parses a String of numbers and/or operators and passes them to the evaluator
+     */
+    public static void parse(String input, PrintWriter pen) throws Exception {
+	// Split input at each whitespace character
+	String[] in = input.split("\\s+");
+	// Evaluate all the characters on the line
+	for (int i = 0; i < in.length; i++) {
+	    terminate = eval(in[i], pen);
+	}// for
+    }// parse
+
+    /**
+     * Evaluates an input, adding it to the stack or executing the appropriate operation
      * @param input
-     * 		an input string representing a single operator or double
-     * @return 
-     * 		a boolean indicating whether the calculations are done
+     *            an input string representing a single operator or double
+     * @return a boolean indicating whether or not to terminate the program
      * @throws Exception
      */
-    public static boolean eval(String input) throws Exception {
+    public static boolean eval(String input, PrintWriter pen) throws Exception {
 	double first = 0;
 	double second = 0;
 	LinkedStackIterator<Double> it = new LinkedStackIterator<Double>(vals);
-	
+
+	// Test for special operations
 	if (input.equals("q")) {
 	    return true;
 	} else if (input.equals("p")) {
@@ -55,49 +70,52 @@ public class RPN {
 		pen.println("Stack empty");
 	    } else {
 		pen.println(vals.peek());
-	    }
+	    }// if
 	} else if (input.equals("s")) {
 	    while (it.hasNext()) {
 		pen.println(it.next());
 	    }
 	} else if (input.equals("c")) {
-	    vals = new LinkedStack<Double>();	    
+	    vals = new LinkedStack<Double>();
+	    // Test for mathematical operators
 	} else if (input.equals("+") || input.equals("/") || input.equals("*")
-		|| input.equals("-") || input.equals("m")) {
+		|| input.equals("-") || input.equals("max") || input.equals("min")) {
 	    try {
 		first = vals.pop();
 	    } catch (Exception e) {
 		pen.println("Not enough items on stack");
 		return false;
-	    }
+	    }// try/catch
 	    try {
 		second = vals.pop();
 	    } catch (Exception e) {
 		vals.push(first);
 		pen.println("Not enough items on stack");
 		return false;
-	    }
+	    }// try/catch
 	    if (input.equals("+")) {
 		vals.push(first + second);
 	    } else if (input.equals("-")) {
 		vals.push(first - second);
 	    } else if (input.equals("/")) {
 		vals.push(first / second);
-	    } else if (input.equals("m")){
+	    } else if (input.equals("max")) {
 		vals.push(Math.max(first, second));
-	    }else{
+	    } else if (input.equals("min")) {
+		vals.push(Math.min(first, second));
+	    } else {
 		vals.push(first * second);
-	    }
-
+	    }// if
 	} else {
+	    // Assume input is a number, try to push it to stack
 	    try {
 		Double d = Double.parseDouble(input);
 		vals.push(d);
 	    } catch (Exception e) {
+		// If parsing input as a double fails, alert user of error
 		pen.println("Unsupported operation: " + input);
-	    }
-	}
-
+	    }// try
+	}// if
 	return false;
-    }
-}
+    }// eval
+}// RPN
